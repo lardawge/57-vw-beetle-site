b928ee5364929513f3066144ab3581a145e5b157
Larry Sprock <larry@lucidbleu.com>
2025-08-13 10:10:47 -0700

    minify assets

--- config.rb
+++ config.rb
@@ -40,7 +40,7 @@ page '/*.txt', layout: false
 
 helpers do
     def gallery_file_name(id, file_name)
-       "#{file_name}#{format_id(id)}" 
+       "#{file_name}#{format_id(id)}"
     end
 
     def format_id(id)
@@ -57,7 +57,7 @@ end
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
